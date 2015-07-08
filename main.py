1ddb8f8207f17ab945eef8a2cba4370abb12bf6e
Mickael Bidon <mbidon@netatmo.com>
2015-07-08 19:04:10 +0200

    Added edition

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 import flask
 import flask.ext.sqlalchemy
-from flask import render_template
+from flask import render_template, redirect
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy import Table, Column, Integer, ForeignKey
+from flask.ext.wtf import Form
+from wtforms import StringField, BooleanField, HiddenField
+from wtforms.validators import DataRequired
 
-database_url = 'sqlite:////home/mogmi/Prog/web/Trombi/test.db'
-
+'''
+    INIT
+'''
 
 # Create the Flask application and the Flask-SQLAlchemy object.
 app = flask.Flask(__name__, static_url_path='/static')
-app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = database_url
+app.config.from_object('config')
+
 db = flask.ext.sqlalchemy.SQLAlchemy(app)
+
+'''
+    TABLES
+'''
 
 class Team(db.Model):
     __tablename__ = 'team'
@@ -29,10 +37,12 @@ class Person(db.Model):
     __tablename__ = 'person'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=False)
+    image = db.Column(db.String(120), unique=False)
     email = db.Column(db.String(120), unique=False)
+    phone = db.Column(db.String(120), unique=False)
     job = db.Column(db.String(120), unique=False)
-    team_id = Column(Integer, ForeignKey('team.id'))
     skype = db.Column(db.String(120), unique=False)
+    team_id = Column(Integer, ForeignKey('team.id'))
 
     def __init__(self, name):
         self.name = name
@@ -40,13 +50,17 @@ class Person(db.Model):
     def __repr__(self):
         return 'Person(' + str(self.id) + ')=' + self.name
 
+'''
+    MAIN
+'''
+
 @app.route("/")
 def main():
     teams = Team.query.all()
     return render_template('index.html', teams=teams)
 
 @app.route('/ajax/createPerson/<id>')
-def command(id=None):
+def create_person(id=None):
     print('Creating a person in team : ' + str(id))
 
     team = Team.query.filter_by(id=id).first()
@@ -60,6 +74,46 @@ def command(id=None):
     print('Person : ' + str(person))
 
     return str(person.id)
+
+
+'''
+    EDIT
+'''
+
+class EditForm(Form):
+    person_id = HiddenField("PERSON_ID")
+    name = StringField('name', default=None)
+    # image = db.Column(db.String(120), unique=False)
+    email = StringField('email', default=None)
+    phone = StringField('phone', default=None)
+    job = StringField('job', default=None)
+    skype = StringField('skype', default=None)
+
+
+@app.route('/ajax/showEdit/<person_id>')
+def show_edit(person_id=None):
+    print('Editing a person person_id : ' + str(person_id))
+    person = Person.query.filter_by(id=person_id).first()
+    form = EditForm(person_id=person_id,name=person.name, email=person.email, phone=person.phone, job=person.job, skype=person.skype)
+    return render_template('form_module.html', person=person, form=form)
+
+@app.route('/editPerson', methods=['GET', 'POST'])
+def modify_person():
+    form = EditForm()
+    if form.validate_on_submit():
+        person_id = form.person_id.data
+        print("person_id : " + person_id)
+
+        person = Person.query.filter_by(id=person_id).first()
+        person.name = form.name.data
+        person.email = form.email.data
+        person.phone = form.phone.data
+        person.job = form.job.data
+        person.skype = form.skype.data
+        db.session.commit()
+
+        return redirect('/')
+
 
 if __name__ == "__main__":
     db.create_all()
