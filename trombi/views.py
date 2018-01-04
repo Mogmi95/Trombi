26af2fc4f8c9c70093b40b04272ed9da4452f5ff
Mickael Bidon <mickael.bidon@netamo.com>
2018-01-04 10:01:37 +0100

    Updated years for calendar
    Really, I should fix this.

--- trombi/views.py
+++ trombi/views.py
@@ -206,7 +206,7 @@ def show_calendar():
     # Persons events
     birthday_events = '['
     arrival_events = '['
-    for year in [2016, 2017]:
+    for year in [2018, 2019]:
         for person in persons:
             if (person.birthday != ''):
                 birth_date = person.birthday
