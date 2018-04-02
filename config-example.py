21a380e11ef41cbb2afa0ec9190244ff584e9142
Mogmi <mogmi95@gmail.com>
2018-04-02 16:07:28 +0200

    Updated the default config file

--- config-example.py
+++ config-example.py
@@ -11,12 +11,9 @@ DEFAULT_FILE_STORAGE = 'filesystem'
 DATABASE_PATH = '/tmp/trombi_database.sqlite'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_PATH
 PHOTOS_FOLDER = os.path.realpath('.') + '/photos/'
+CSV_FOLDER = os.path.realpath('.') + '/data/'
 WEBSITE_URL = 'http://localhost:5000'
 PORT = 5000
-
-DATABASE_PERSONS_FILE = 'data/example-persons.csv'
-DATABASE_TEAMS_FILE = 'data/example-teams.csv'
-DATABASE_ROOMS_FILE = 'data/example-rooms.csv'
 
 # DATABASE SAVES
 DATABASE_SAVES_DIRECTORY = os.path.realpath('.') + '/backups'
